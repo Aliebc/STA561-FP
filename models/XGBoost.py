d7e6ef8e97ca49cb92098293b2d2e4cca3a21e95
Bohan Wang <wangbohan0608@gmail.com>
2025-04-26 15:25:32 -0400

    Add XGBoost Model

--- models/XGBoost.py
+++ models/XGBoost.py
@@ -1,18 +1,28 @@
+"""
+Created on Fri Apr 25 12:21:39 2025
+
+@author: wangb
+"""
+
 from ._models import register_model
 from functools import partial
+from sklearn.model_selection import GridSearchCV
 from xgboost import XGBClassifier
 
 register_model(
-    model_name="XGBoost",
-    model_description="XGBoost Classifier (100 trees, max_depth=6)",
+    model_name="XGBoost_Tuned",
+    model_description="XGBoost Classifier with GridSearchCV",
     model_class=partial(
-        XGBClassifier,
-        n_estimators=100,       
-        learning_rate=0.1,      
-        max_depth=6,            
-        use_label_encoder=False,
-        eval_metric='mlogloss', 
-        random_state=42         
+        GridSearchCV,
+        estimator=XGBClassifier(use_label_encoder=False, eval_metric='logloss'),
+        param_grid={
+            'n_estimators': [100, 200, 300],
+            'max_depth': [4, 6, 8, 10],
+            'learning_rate': [0.01, 0.05, 0.1],
+            'subsample': [0.8, 1.0]
+        },
+        cv=5,
+        scoring='accuracy',
+        n_jobs=-1
     )
 )
-
