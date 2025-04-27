584e679a15961a1136521209da87e5e9ebb056c7
Yi <yx.thu@outlook.com>
2025-04-27 03:03:14 -0400

    change api url

--- webserver.py
+++ webserver.py
@@ -129,4 +129,4 @@ def eva():
     return jsonify({"message": "Success", "data": ret_data}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=55322)
